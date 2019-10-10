fa4773ba7506543bd2be5725de38dfaa753855e9
jody-davids-07 <Your Actual Email>
2019-10-10 11:42:30 +0100

    Added another base connection with all database login information -base_class.php

---
+++ config/base_class.php
@@ -0,0 +1,10 @@
+<?php
+//name of the server
+define('DB_HOST_CLASS','167.71.74.228');
+//Name of the System Admo
+define('DB_USERNAME_CLASS','jody');
+//name of the Server password
+define('DB_PASS_CLASS','chose90vanish77citric');
+//name of the database to connect - to_do_database. 
+define('DB_NAME_CLASS','2019BWebIntensive_jody');
+?>
