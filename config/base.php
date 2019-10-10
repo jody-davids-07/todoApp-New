6e9bfd3b847af6af413380e144ad53f8a5cc646e
jody-davids-07 <Your Actual Email>
2019-10-10 11:50:00 +0100

    Changed default information to remote databases login information

--- config/base.php
+++ config/base.php
@@ -1,10 +1,10 @@
 <?php
 //name of the server
-define('DB_HOST','localhost');
+define('DB_HOST','167.71.74.228');
 //Name of the System Admo
-define('DB_USERNAME','navadmin');
+define('DB_USERNAME','jody');
 //name of the Server password
-define('DB_PASS','navpass');
+define('DB_PASS','chose90vanish77citric');
 //name of the database to connect - to_do_database. 
-define('DB_NAME','navtodo');
+define('DB_NAME','2019BWebIntensive_jody');
 ?>
